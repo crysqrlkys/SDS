08fc11fdae39b176cdb3707845b0bcefa32714d5
BlackLionz <shotaik@mail.ru>
2019-12-01 23:25:45 +0300

    Fix login view

--- donutsender/core/models.py
+++ donutsender/core/models.py
@@ -1,37 +1,14 @@
 from decimal import Decimal
 
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import DO_NOTHING
 from django.utils.translation import gettext as _
 
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def create_user(self, username, email, password=None):
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password):
-        user = self.create_user(
-            username=username,
-            email=email,
-            password=password,
-        )
-        user.staff = True
-        user.admin = True
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser):
+class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(_('email address'), unique=True)
 
@@ -47,8 +24,6 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-
-    objects = UserManager()
 
     def __str__(self):
         return self.username
--- donutsender/core/serializers.py
+++ donutsender/core/serializers.py
@@ -1,18 +1,34 @@
-from django.contrib.auth import get_user_model
+from donutsender.core.models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ('id', 'url', 'username', 'email', 'avatar', 'password')
         extra_kwargrs = {
             'password': {'write_only': True},
             'id': {'read_only': True},
         }
 
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def save(self, request):
+        data = request.data
+        if self.instance:
+            self.instance = self.update(instance=self.instance, **data)
+        else:
+            user_ser = UserSerializer(data=data)
+            user_ser.is_valid()
+            self.instance = self.create(user_ser.validated_data)
+        return self.instance
+
 
 class LoginUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = ('username', 'email')
+        model = User
+        fields = ('username', 'password')
