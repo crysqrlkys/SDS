a46d9863856a00aa368ccc8b9032c501810f2668
BlackLionz <shotaik@mail.ru>
2019-12-07 18:25:52 +0300

    Fix permissions

--- tools/custom_permissions.py
+++ tools/custom_permissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsOwnerOrAdmin(BasePermission):
     """Access have only author and staff"""
     def has_object_permission(self, request, view, obj):
-        return bool(request.user.is_staff or request.user.id == obj.created_by.id)
+        return bool(request.user.is_staff or request.user.id == obj.from_user.id)
